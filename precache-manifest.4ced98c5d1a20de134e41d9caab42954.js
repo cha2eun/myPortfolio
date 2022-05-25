b6fe1bd41147d944d5f57c914bfd5516c8cd4b92
cha2eun <jan9chae2un@gmail.com>
2022-05-25 22:52:50 +0900

    Updates

---
+++ precache-manifest.4ced98c5d1a20de134e41d9caab42954.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcb349a821508e56f094634844c4fca5",
+    "url": "/myPortfolio/index.html"
+  },
+  {
+    "revision": "0754b85bc633a12f2739",
+    "url": "/myPortfolio/static/css/main.7a95f607.chunk.css"
+  },
+  {
+    "revision": "f0585afd58bab4c12498",
+    "url": "/myPortfolio/static/js/2.9a2180c5.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/myPortfolio/static/js/2.9a2180c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0754b85bc633a12f2739",
+    "url": "/myPortfolio/static/js/main.50b774ac.chunk.js"
+  },
+  {
+    "revision": "1d06900a7f3ea2b490bb",
+    "url": "/myPortfolio/static/js/runtime-main.4b3e8bd9.js"
+  },
+  {
+    "revision": "b88924d5d914bf9f3885f87c2ed76477",
+    "url": "/myPortfolio/static/media/sad-icon.b88924d5.png"
+  },
+  {
+    "revision": "b9d7d448a09d6aacb45d21aab2c6fd53",
+    "url": "/myPortfolio/static/media/your photo.b9d7d448.png"
+  }
+]);
